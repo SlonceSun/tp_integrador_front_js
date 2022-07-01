fe191e7f8458998c46bb9934399e9490a5345d05
Solange <solisis86@gmail.com>
2022-07-01 19:22:30 -0300

    Correccion form submit hacia refresh

--- js/scripts.js
+++ js/scripts.js
@@ -4,79 +4,80 @@ var tktPrice = 200;
 
 /* DESCUENTOS SEGUN CATEGORIAS */
 
-var dtoStudent = 0.80; /* Esto es un 80% */
-var dtoTrainee = 0.50; /* Esto es un 50% */
+var dtoStudent = 0.8; /* Esto es un 80% */
+var dtoTrainee = 0.5; /* Esto es un 50% */
 var dtoJunior = 0.15; /* Esto es un 15% */
-
 
 /* VARIABLES DEL FORM */
 
-var nombre          = document.getElementById ("firstName");
-var apellido        = document.getElementById ("lastName");
-var email           = document.getElementById ("email1");
-var cantEntradas    = document.getElementById ("cantEntradas");
-var categoria       = document.getElementById ("categElegida");
-
+var nombre = document.getElementById('firstName');
+var apellido = document.getElementById('lastName');
+var email = document.getElementById('email1');
+var cantEntradas = document.getElementById('cantEntradas');
+var categoria = document.getElementById('categElegida');
 
 /* PARA QUE FIGUREN LOS ERRORES AL PONER SUBMIT, SI NO FUE COMPLETADO O UN CAMPO NO CORRESPONDE SU CONTENIDO */
 (function () {
-    'use strict'
-  
-    /* TOMA TODOS LOS FORMS QUE LLEVARAN FORMATO DE INVALIDO */
-    var forms = document.querySelectorAll('.needs-validation')
-  
-  
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+	'use strict';
 
+	/* TOMA TODOS LOS FORMS QUE LLEVARAN FORMATO DE INVALIDO */
+	var forms = document.querySelectorAll('.needs-validation');
 
+	Array.prototype.slice.call(forms).forEach(function (form) {
+		form.addEventListener(
+			'submit',
+			function (event) {
+				if (!form.checkValidity()) {
+					event.preventDefault();
+					event.stopPropagation();
+				}
 
-  /* HACE CALCULO DE VALOR TOTAL x CANT TICKETS CON SU CORRESPONDIENTE DESCUENTO */
+				form.classList.add('was-validated');
+			},
+			false
+		);
+	});
+})();
 
-  function montoTotalPagar(){
-    let totalValorTkt = (cantEntradas.value) * tktPrice;
+var formu = document.getElementById('form_tkts_comp');
+formu.addEventListener('submit', function (evt) {
+	evt.preventDefault();
+	evt.stopPropagation();
+});
 
-    if (categoria.value == 0) { 
-      totalValorTkt = totalValorTkt;
-    }
+/* HACE CALCULO DE VALOR TOTAL x CANT TICKETS CON SU CORRESPONDIENTE DESCUENTO */
 
-    if (categoria.value == 1) {
-      totalValorTkt = totalValorTkt * ((1-dtoStudent)).toFixed(2); /* NOTA: Coloque toFixed porque sino por algun motivo el valor total daba 39.999999 */ 
-    }
+function montoTotalPagar() {
+	let totalValorTkt = cantEntradas.value * tktPrice;
 
-   
+	if (categoria.value == 0) {
+		totalValorTkt = totalValorTkt;
+	}
 
-    if (categoria.value == 2) {
-      totalValorTkt = totalValorTkt * (1-dtoTrainee);
-    }
-    
-    if (categoria.value == 3) {
-      totalValorTkt = totalValorTkt * (1-dtoJunior);
-    }
+	if (categoria.value == 1) {
+		totalValorTkt =
+			totalValorTkt *
+			(1 - dtoStudent).toFixed(
+				2
+			); /* NOTA: Coloque toFixed porque sino por algun motivo el valor total daba 39.999999 */
+	}
 
-    totalPagar.innerHTML = totalValorTkt;
+	if (categoria.value == 2) {
+		totalValorTkt = totalValorTkt * (1 - dtoTrainee);
+	}
 
-  }
+	if (categoria.value == 3) {
+		totalValorTkt = totalValorTkt * (1 - dtoJunior);
+	}
 
-
-btnSubmitForm.addEventListener ('click', montoTotalPagar);
-
-function clearForm (){
-
-  totalPagar.innerHTML = "";
-  document. getElementById("form_tkts_comp"). className = "needs-validation"
+	totalPagar.innerHTML = totalValorTkt;
 }
 
+btnSubmitForm.addEventListener('click', montoTotalPagar);
 
+function clearForm() {
+	totalPagar.innerHTML = '';
+	document.getElementById('form_tkts_comp').className = 'needs-validation';
+}
 
-btnBorraForm.addEventListener ('click', clearForm);
+btnBorraForm.addEventListener('click', clearForm);
